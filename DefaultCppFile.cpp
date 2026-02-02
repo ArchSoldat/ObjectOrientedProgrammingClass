0cb45c9c33a4789bc91a235a2aea174f37f2c5ad
ArchSoldat <chadtabor@gmail.com>
2026-02-02 13:29:04 -0600

    Added default cpp file

---
+++ DefaultCppFile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// The main function is the entry point of every C++ program.
+int main()
+{
+    // Print a message to the console
+    std::cout << "Hello, Visual Studio Code with MSVC!" << std::endl;
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
+
